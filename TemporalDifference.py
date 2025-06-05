e2e0459c286c4b691cb40c1f1cd84be8cd27739a
nikoczernin <e11721138@student.tuwien.ac.at>
2025-06-05 08:48:02 +0200

    added vis

--- TemporalDifference.py
+++ TemporalDifference.py
@@ -1,20 +1,23 @@
 from pprint import pprint
 from random import random
 
+from matplotlib import pyplot as plt
+
 from Bot import Bot
 from CliffWalking import CliffWalking
 from GridWorld import GridWorld
+from utils import plot_blockwise_means_line_graph, plot_line_graph
 
 
-def SARSA(bot:Bot, alpha=.5, epsilon=.1, gamma=1, num_episodes=1000, expected=False):
-    if expected: ("Performing Expected-SARSA...")
-    else: ("Performing SARSA...")
+def SARSA(bot:Bot, alpha=.5, epsilon=.1, gamma=1.0, num_episodes=1000, expected=False):
+    if expected: print("Performing Expected-SARSA...")
+    else: print("Performing SARSA...")
     # init action-value function (tabular, finite)
     # should we consider all states beforehand?
-    # Q = {}
     Q = {s:{a:0 for a in bot.env.actions} for s in bot.env.state_generator()}
-
+    total_rewards = [] # keep a list of total rewards for each episode
     for k in range(num_episodes):
+        episode_reward = 0
         # reset the env
         bot.env.reset()
         # before every episode: set a policy according to Q
@@ -33,6 +36,7 @@ def SARSA(bot:Bot, alpha=.5, epsilon=.1, gamma=1, num_episodes=1000, expected=Fa
             # take action a_t and receive s_t_1 and reward r
             s_t_1 = bot.env.apply_action(s_t, a_t)
             r = bot.env.get_reward(s_t, a_t, s_t_1)
+            episode_reward += r
             # choose action a_t_1 from s_t_1 using e-greedy-policy
             a_t_1 = bot.pick_action(s_t_1, epsilon)
             # perform update of Q using SARSA update formula
@@ -49,24 +53,26 @@ def SARSA(bot:Bot, alpha=.5, epsilon=.1, gamma=1, num_episodes=1000, expected=Fa
 
             # set s_t and a_t to the new state and action (we are doing on-policy control)
             s_t, a_t = s_t_1, a_t_1
+        total_rewards.append(episode_reward)
 
+    return Q, total_rewards
 
-    return Q
-
-def expected_SARSA(bot:Bot, alpha=.5, epsilon=.1, gamma=1, num_episodes=1000):
+def expected_SARSA(bot:Bot, alpha=.5, epsilon=.1, gamma=1.0, num_episodes=1000):
     return SARSA(bot, alpha, epsilon, gamma, num_episodes, expected=True)
 
 
-def Q_Learning(bot:Bot, alpha=.5, epsilon=.1, gamma=1, num_episodes=1000):
+def Q_Learning(bot:Bot, alpha=.5, epsilon=.1, gamma=1.0, num_episodes=1000):
     print("Performing Q-Learning...")
     # init action-value function (tabular, finite)
     # should we consider all states beforehand?
     # Q = {}
     Q = {s: {a: 0 for a in bot.env.actions} for s in bot.env.state_generator()}
+    total_rewards = [] # keep a list of total rewards for each episode
 
     for k in range(num_episodes):
         # reset the env
         bot.env.reset()
+        episode_reward = 0
         for s in Q.keys():
             # update the policy of the bot
             bot.policy_set_action(s, max(Q[s], key=Q[s].get))
@@ -82,6 +88,7 @@ def Q_Learning(bot:Bot, alpha=.5, epsilon=.1, gamma=1, num_episodes=1000):
             # take action a_t and receive s_t_1 and reward r
             s_t_1 = bot.env.apply_action(s_t, a_t)
             r = bot.env.get_reward(s_t, a_t, s_t_1)
+            episode_reward += r
             # choose action a_t_1 from s_t_1 that maximizes Q[s_t_1]
             a_t_1 = max(Q[s_t], key=Q[s_t].get)
             # perform update of Q using SARSA update formula
@@ -94,21 +101,21 @@ def Q_Learning(bot:Bot, alpha=.5, epsilon=.1, gamma=1, num_episodes=1000):
 
             # set s_t to the new state (off-policy control so a_t_1 does not get used)
             s_t = s_t_1
-    return Q
+        total_rewards.append(episode_reward)
+    return Q, total_rewards
 
 
 
-def Double_Q_Learning(bot:Bot, alpha=.5, epsilon=.1, gamma=1, num_episodes=1000):
+def Double_Q_Learning(bot:Bot, alpha=.5, epsilon=.1, gamma=1.0, num_episodes=1000):
     print("Performing Double-Q-Learning...")
-    # init action-value function (tabular, finite)
-    # should we consider all states beforehand?
-    # Q = {}
+    # init 2 action-value functions (tabular, finite)
     Q1 = {s: {a: 0 for a in bot.env.actions} for s in bot.env.state_generator()}
     Q2 = {s: {a: 0 for a in bot.env.actions} for s in bot.env.state_generator()}
-
+    total_rewards = []
     for k in range(num_episodes):
         # reset the env
         bot.env.reset()
+        episode_reward = 0
         for s in Q1.keys():
             # update the policy of the bot
             # get the action with the highest mean action-value
@@ -126,6 +133,7 @@ def Double_Q_Learning(bot:Bot, alpha=.5, epsilon=.1, gamma=1, num_episodes=1000)
             # take action a_t and receive s_t_1 and reward r
             s_t_1 = bot.env.apply_action(s_t, a_t)
             r = bot.env.get_reward(s_t, a_t, s_t_1)
+            episode_reward += r
             # generate random uniform number, with a 50/50 chance swap them
             u = random()
             if u < .5:
@@ -142,27 +150,82 @@ def Double_Q_Learning(bot:Bot, alpha=.5, epsilon=.1, gamma=1, num_episodes=1000)
 
             # set s_t to the new state (off-policy control so a_t_1 does not get used)
             s_t = s_t_1
+        total_rewards.append(episode_reward)
     # compute an average Q
     Q = {s:{a:(Q1[s][a] + Q2[s][a])/2 for a in Q1[s].keys()} for s in Q1.keys()}
-    return Q
+    return Q, total_rewards
 
 
-def test_grid_world(algo=SARSA):
+def Speedy_Q_Learning(bot:Bot, alpha=None, gamma=1.0, epsilon=.1,  num_episodes=1000):
+    print("Performing Speedy-Q-Learning...")
+    # init action-value function (tabular, finite)
+    Q = {s: {a: 0 for a in bot.env.actions} for s in bot.env.state_generator()}
+    total_rewards = []
+    for k in range(num_episodes):
+        # reset the env
+        bot.env.reset()
+        episode_reward = 0
+        for s in Q.keys():
+            # update the policy of the bot
+            bot.policy_set_action(s, max(Q[s], key=Q[s].get))
+
+        # initialize s
+        s_t = bot.env.starting_state
+        for t in range(bot.T):
+            # if s is terminal, terminate the episode
+            if bot.env.state_is_terminal(s_t):
+                break
+            # set the learning_rate (dep on t)
+            alpha_t = 1 / (t + 1)
+            # choose action a from s_t using e-greedy policy
+            a_t = bot.pick_action(s_t, epsilon)
+            # take action a_t and receive s_t_1 and reward r
+            s_t_1 = bot.env.apply_action(s_t, a_t)
+            r = bot.env.get_reward(s_t, a_t, s_t_1)
+            episode_reward += r
+            # choose action a_t_1 from s_t_1 that maximizes Q[s_t_1]
+            a_t_1 = max(Q[s_t], key=Q[s_t].get)
+            # perform update of Q using SARSA update formula
+            # before updating, save Q,r as Q_prev,r_prev
+            Q_prev = Q.copy()
+            r_prev = r
+            # if the next state is terminal, its future reward is zero
+            if bot.env.state_is_terminal(s_t_1):
+                BQt = r
+                BQt_1 = r_prev
+                Q[s_t][a_t] = Q[s_t][a_t] + alpha_t * (BQt_1 - Q[s_t][a_t]) + (1 - alpha_t) * (BQt - BQt_1)
+
+            else:
+                BQt = r + gamma * Q[s_t_1][a_t_1]
+                BQt_1 = r_prev + gamma * Q_prev[s_t_1][max(Q_prev[s_t_1], key=Q_prev[s_t_1].get)]
+                Q[s_t][a_t] = Q[s_t][a_t] + alpha_t * (BQt_1 - Q[s_t][a_t]) + (1-alpha_t) * (BQt - BQt_1)
+
+            # set s_t to the new state (off-policy control so a_t_1 does not get used)
+            s_t = s_t_1
+        total_rewards.append(episode_reward)
+    return Q, total_rewards
+
+
+def test_grid_world(algo=SARSA, print_q=False, alpha = .2, epsilon = .3, gamma = .9, num_episodes=1000):
+    print(f"Testing {algo.__name__} on Grid-World...")
     # init env
     h, w = 4, 4  # grid size
     env = GridWorld(h, w, terminal_states=[(0, 0), (w - 1, h - 1)], starting_state=(2, 1))
     print(env)
     # init bot
     bot = Bot(env)
-    Q = algo(bot, alpha=.1, epsilon=.1, gamma=1, num_episodes=10000)
-    pprint(Q)
+    Q, total_rewards = algo(bot, alpha=alpha, epsilon=epsilon, gamma=gamma, num_episodes=num_episodes)
+    if print_q: pprint(Q)
     bot.draw_policy()
     # pprint(bot.policy)
     bot.make_test_runs(1000)
+    # plot_line_graph(total_rewards)
+    plot_blockwise_means_line_graph(total_rewards, title=f"Total Rewards {algo.__name__}", xlabel="episodes", ylabel="reward")
     print("-"*50)
 
 
-def test_cliff_walking(algo=SARSA):
+def test_cliff_walking(algo=SARSA, print_q=False, alpha = .2, epsilon = .3, gamma = .9, num_episodes=10000):
+    print(f"Testing {algo.__name__} on Cliff-Walking...")
     # tests Monte Carlo control on cliff walking scenario
     h, w = 4, 5
     cliffs = [(1, 1), (1, 2), (1, 3)]
@@ -172,21 +235,23 @@ def test_cliff_walking(algo=SARSA):
     alpha = .2
     epsilon = .3
     gamma = .9
-    Q = algo(bot, alpha=alpha, epsilon=epsilon, gamma=gamma, num_episodes=10000)
+    Q, total_rewards = algo(bot, alpha=alpha, epsilon=epsilon, gamma=gamma, num_episodes=num_episodes)
     pprint("Q")
-    pprint(Q)
+    if print_q: pprint(Q)
     print("policy")
-    pprint(bot.policy)
+    if print_q: pprint(bot.policy)
     bot.draw_policy()
     bot.make_test_runs(1000)
+    plot_blockwise_means_line_graph(total_rewards, title=f"Total Rewards {algo.__name__}", xlabel="episodes", ylabel="reward")
     print("-"*50)
 
 
 if __name__ == '__main__':
-    # test_grid_world(SARSA)
+    test_grid_world(SARSA)
     # test_grid_world(expected_SARSA)
     # test_grid_world(Q_Learning)
     # test_cliff_walking(SARSA)
     # test_cliff_walking(expected_SARSA)
     # test_cliff_walking(Q_Learning)
-    test_cliff_walking(Double_Q_Learning)
+    # test_cliff_walking(Double_Q_Learning)
+    # test_cliff_walking(Speedy_Q_Learning)
--- utils.py
+++ utils.py
@@ -1,4 +1,6 @@
 import matplotlib.pyplot as plt
+import numpy as np
+
 
 
 def plot_line_graph(*values_lists, title="Line Graph", xlabel="X-axis", ylabel="Y-axis", labels=None):
@@ -6,14 +8,29 @@ def plot_line_graph(*values_lists, title="Line Graph", xlabel="X-axis", ylabel="
         if labels:
             plt.plot(values, label=labels[i])
         else:
-            plt.plot(values)  # no label if not provided
-
+            plt.plot(values)
     plt.title(title)
     plt.xlabel(xlabel)
     plt.ylabel(ylabel)
     plt.grid(True)
-
     if labels:
         plt.legend()
-
     plt.show()
+
+
+def plot_blockwise_means_line_graph(values_list, k=20,
+                                     title="Line Graph", xlabel="X-axis", ylabel="Y-axis", labels=None):
+    n = len(values_list)
+    step_size = n//k
+    means = [np.mean(values_list[i : i + step_size]) for i in range(0, n, step_size)]
+    plt.plot(means, label=labels)
+    plt.title(title)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.grid(True)
+    if labels:
+        plt.legend()
+    plt.xticks(np.arange(0, k+1, 5), np.arange(0, k+1, 5)*step_size)
+    plt.show()
+
+
